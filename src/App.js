e9b8df9755f3d5aa927a2205120c965925c12bef
unknown <6804977@device.hmc.co.kr>
2021-09-03 15:35:55 +0900

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
-import './App.css';
-import MainPage from "./main/index.js"
-import {Switch, Route} from 'react-router-dom';
-import UploadPage from "./upload"
-import ProductPage from "./product"
-
+import "./App.css";
+import MainPage from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <div>
-    <Switch>
-      <Route exact={true} path={"/"}>
-        <MainPage />
-      </Route>
-      {/* :id -> 자유롭게 id에 숫자를 입력해보면 아래 ProductPage 경로를 타게 됨 */}
-      <Route exact={true} path="/product/:id">
-        <ProductPage />
-      </Route>
-      <Route exact={true} path="/upload">
-        <UploadPage />
-      </Route>
-    </Switch>
-    </div>; 
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPage />
+        </Route>
+        {/* :id -> 자유롭게 id에 숫자를 입력해보면 아래 ProductPage 경로를 타게 됨 */}
+        <Route exact={true} path="/product/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/main/index.js
+++ src/main/index.js
@@ -1,66 +1,73 @@
-import './index.css';
+import "./index.css";
 import axios from "axios";
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function MainPage(){
-// 통신 결과를 아래 card에 넣어 주기위해 state 사용
-    const [products, setProducts] = React.useState([]);
-// 네트워크 통신
-//  state가 변화됨에 따라 무한 반복되는 것을 방지하는 함수 : useEffect
-React.useEffect(
-    function(){
-        axios.get("https://0a6a94d4-5496-4870-a65b-ba04a9d2458f.mock.pstmn.io/products")
-        .then(function(result){
-            const products = result.data.product;
-            setProducts(products);
-        }).catch(function(error){
-         console.log('에러 발생 :',error);
-        })
-    },[]
-    )
+function MainPage() {
+  // 통신 결과를 아래 card에 넣어 주기위해 state 사용
+  const [products, setProducts] = React.useState([]);
+  // 네트워크 통신
+  //  state가 변화됨에 따라 무한 반복되는 것을 방지하는 함수 : useEffect
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://0a6a94d4-5496-4870-a65b-ba04a9d2458f.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.product;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.log("에러 발생 :", error);
+      });
+  }, []);
 
-
-
-    return (
+  return (
     <div>
-            <div id='header'>
-                <div id='header-area'>
-                <img src="images/icons/logo.png" />
-                </div>
-            </div>
-            <div id="body">
-                <div id="banner">
-                <img src="images/banners/banner1.png"/>
-                </div>
-                <h1>판매되는 상품들</h1>
-                <div id="product-list">
-                    {/* jsx 문법을 사용해서 구축 */}
-                    {products.map(function(product,index){
-                            return(
-                                <div className="product-card">
-                                <Link className="product-link" to={`/product/${index}`}>
-                                    <div>
-                                        <img className="product-img" src={product.imageurl} alt="profile"/>
-                                    </div>
-                                    <div className="product-contents">
-                                    <span className="product-name">{product.name}</span>
-                                    <span className="product-price">{product.price}원</span>
-                                        <div className="product-seller">
-                                          <img className="product-avatar" src="images/icons/avatar.png" alt="profile"/>
-                                          <span>{product.seller}</span>
-                                        </div>
-                                    </div>
-                                </Link>
-                                </div>
-                            );
-                        })}
-                         
-                </div>
-            </div>
-        <div id='footer'></div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {/* jsx 문법을 사용해서 구축 */}
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <Link className="product-link" to={`/product/${index}`}>
+                  <div>
+                    <img
+                      className="product-img"
+                      src={product.imageurl}
+                      alt="profile"
+                    />
+                  </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                        alt="profile"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
     </div>
-    )
+  );
 }
 
 export default MainPage;
